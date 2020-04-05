1f944a6664cc46acf19fced9cfa838d966e2cf52
aroma1994 <fabriccanbleed@gmail.com>
2020-04-05 16:32:05 +0700

    Add :disown

--- polybar-windows.sh
+++ polybar-windows.sh
@@ -4,6 +4,6 @@ active_window=$(xprop -root _NET_ACTIVE_WINDOW|cut -d ' ' -f 5|sed -e 's/../0&/2
 
 current_display=$(wmctrl -d|grep "*"|awk '{print $1}')
 
-current_windows=$(wmctrl -lx|awk -v current_display="$current_display" -v active_window="$active_window" '{if ($2==current_display) {if ($1==active_window) $3="#"$3; split($3,window_title,"."); print "%{A1: wmctrl -ia "$1" :}"window_title[1]"%{A}" } }')
+current_windows=$(wmctrl -lx|awk -v current_display="$current_display" -v active_window="$active_window" '{if ($2==current_display) {if ($1==active_window) $3="#"$3; split($3,window_title,"."); print "%{A1: wmctrl -ia "$1" & disown:}"window_title[1]"%{A}" } }')
 
 echo $current_windows
