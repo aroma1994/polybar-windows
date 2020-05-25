1128826dfff5dd6e17e7b4a2072f40b92c61a982
alnj <kokone@takat.su>
2020-05-25 10:46:18 +0200

    formatting

--- polywins.sh
+++ polywins.sh
@@ -17,12 +17,12 @@ resize_increment=30
 
 
 # Setup
-wm_border_width=1 # setting this might be required for accurate resize position
-active_display=$(wmctrl -d | awk '/\*/ {print $1}')
-active_window=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print "0x0"substr($5,3)}')
+wm_border_width=0 # setting this might be required for accurate resize position
 active_window_left="%{F$text_color}%{+u}%{u$underline_color}"
 active_window_right="%{-u}%{F-}"
 
+active_display=$(wmctrl -d | awk '/\*/ {print $1}')
+active_window=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print "0x0"substr($5,3)}')
 
 # On-click actions
 case "$1" in
@@ -66,7 +66,6 @@ case "$1" in
 		exit
 	;;
 	*) ;;
-
 esac
 
 # Generating the window list
@@ -90,24 +89,24 @@ window_list=$(wmctrl -lx|awk -vORS="" -vOFS="" \
 			lastitem=split($3,classname_and_class,".")
 			displayed_name = classname_and_class[lastitem]
 		}
-		else if ( display == "window_classname" ) {
+		else if (display == "window_classname") {
 			split($3,classname_and_class,".")
 			displayed_name = classname_and_class[1]
 		}
-		else if ( display == "window_title" ) {
+		else if (display == "window_title") {
 			title = ""; for (i = 5; i <= NF; i++) {
 				title = title $i; if (i != NF) { title = title " "} }
 			displayed_name = title
 		}
 
-		if      ( case == "lower" ) { displayed_name=tolower(displayed_name) }
-		else if ( case == "upper" ) { displayed_name=toupper(displayed_name) }
+		if      (case == "lower") { displayed_name=tolower(displayed_name) }
+		else if (case == "upper") { displayed_name=toupper(displayed_name) }
 
-		if ( length(displayed_name) > char_limit ) {
+		if (length(displayed_name) > char_limit) {
 			displayed_name = substr(displayed_name,1,char_limit)"…"
 		}
 
-		if ( add_spaces == "true" ) { displayed_name = " "displayed_name" " }
+		if (add_spaces == "true") { displayed_name = " "displayed_name" " }
 
 		if ($1==active_window) {
 			displayed_name=active_left displayed_name active_right
