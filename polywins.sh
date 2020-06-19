f533f3d9f49b3c569f48db1de00336f7df593088
alnj <kokone@takat.su>
2020-06-19 18:14:41 +0200

    only update on active window change or window opening/closing. refactor in pure sh, no awk

--- polywins.sh
+++ polywins.sh
@@ -4,150 +4,207 @@
 # SETTINGS {{{ ---
 
 active_text_color="#250F0B"
-active_underline="true"
-active_underline_color="#E7A09E"
+active_bg=
+active_underline="#ECB3B2"
+
 inactive_text_color="#250F0B"
-inactive_underline="false"
-inactive_underline_color="#F1EF7D"
+inactive_bg=
+inactive_underline=
+
 separator="·"
 show="window_class" # options: window_title, window_class, window_classname
-char_limit=20 # useful with window_title
-max_windows="15" # maximum number of displayed windows
-char_case="normal" # options: normal, upper, lower
+forbidden_classes="Polybar Conky Gmrun"
+
+char_limit=20
+max_windows=15
+char_case="normal" # normal, upper, lower
 add_spaces="true"
-resize_increment=30
-wm_border_width=0 # setting this might be required for accurate resize position
+resize_increment=16
+wm_border_width=1 # setting this might be required for accurate resize position
 
 # --- }}}
 
 
 
 # Setup
-actv_win_left="%{F$active_text_color}"
-actv_win_right="%{F-}"
-inactv_win_left="%{F$inactive_text_color}"
-inactv_win_right="%{F-}"
+active_left="%{F$active_text_color}"
+active_right="%{F-}"
+inactive_left="%{F$inactive_text_color}"
+inactive_right="%{F-}"
 separator="%{F$inactive_text_color}$separator%{F-}"
 
-if [ $active_underline = "true" ]; then
-	actv_win_left="${actv_win_left}%{+u}%{u$active_underline_color}"
-	actv_win_right="%{-u}${actv_win_right}"
+if [ -n "$active_underline" ]; then
+	active_left="${active_left}%{+u}%{u$active_underline}"
+	active_right="%{-u}${active_right}"
 fi
 
-if [ $inactive_underline = "true" ]; then
-	inactv_win_left="${inactv_win_left}%{+u}%{u$inactive_underline_color}"
-	inactv_win_right="%{-u}${inactv_win_right}"
+if [ -n "$active_bg" ]; then
+	active_left="${active_left}%{B$active_bg}"
+	active_right="%{B-}${active_right}"
 fi
 
-active_workspace=$(wmctrl -d | awk '/\*/ {print $1}')
-active_window=$(xprop -root _NET_ACTIVE_WINDOW | awk '{print "0x0"substr($5,3)}')
+if [ -n "$inactive_underline" ]; then
+	inactive_left="${inactive_left}%{+u}%{u$inactive_underline}"
+	inactive_right="%{-u}${inactive_right}"
+fi
+
+if [ -n "$inactive_bg" ]; then
+	inactive_left="${inactive_left}%{B$inactive_bg}"
+	inactive_right="%{B-}${inactive_right}"
+fi
 
 # On-click actions
 case "$1" in
-raise_or_minimize)
-	if [ "${active_window}" = "${2}" ]; then
-		wmctrl -ir "$2" -b toggle,hidden
-	else
+	raise_or_minimize)
+		if [ "$active_window" = "$2" ]; then
+			wmctrl -ir "$2" -b toggle,hidden
+		else
+			wmctrl -ia "$2"
+		fi
+		;;
+
+	close)
+		wmctrl -ic "$2"
+		;;
+
+	slop_resize)
 		wmctrl -ia "$2"
-	fi
-	;;
-close)
-	wmctrl -ic "$2"
-	;;
-slop_resize)
-	wmctrl -ia "$2"
-	wmctrl -ir "$2" -e "$(slop -f 0,%x,%y,%w,%h)"
-	;;
-increment_size)
-	wmctrl -ir "$2" -e "$(wmctrl -G -l |
-		awk -v i="$resize_increment" \
-		    -v b="$wm_border_width" \
-		    -v win="$2" \
-		'$1 ~ win {print "0,"$3-b*2-i/2","$4-b*2-i/2","$5+i","$6+i}')"
-	;;
-decrement_size)
-	wmctrl -ir "$2" -e "$(wmctrl -G -l |
-		awk -v i="$resize_increment" \
-		    -v b="$wm_border_width" \
-		    -v win="$2" \
-		'$1 ~ win {print "0,"$3-b*2+i/2","$4-b*2+i/2","$5-i","$6-i}')"
-	;;
+		wmctrl -ir "$2" -e "$(slop -f 0,%x,%y,%w,%h)"
+		;;
+
+	increment_size)
+		while IFS="[ .]" read -r wid ws wx wy ww wh _; do
+			test "$wid" != "$2" && continue
+			x=$(( wx - wm_border_width*2 - resize_increment/2  ))
+			y=$(( wy - wm_border_width*2 - resize_increment/2  ))
+			w=$(( ww + resize_increment ))
+			h=$(( wh + resize_increment ))
+		done <<-EOF
+		$(wmctrl -lG)
+		EOF
+
+		wmctrl -ir "$2" -e "0,$x,$y,$w,$h"
+		;;
+
+	decrement_size)
+		while IFS="[ .]" read -r wid ws wx wy ww wh _; do
+			test "$wid" != "$2" && continue
+			x=$(( wx - wm_border_width*2 + resize_increment/2  ))
+			y=$(( wy - wm_border_width*2 + resize_increment/2  ))
+			w=$(( ww - resize_increment ))
+			h=$(( wh - resize_increment ))
+		done <<-EOF
+		$(wmctrl -lG)
+		EOF
+
+		wmctrl -ir "$2" -e "0,$x,$y,$w,$h"
+		;;
 esac
 
+# Exit without making windowlist if script is called for on-click actions
 if [ -n "$2" ]; then exit; fi
 
 
-# Generating the window list
-window_list=$(wmctrl -lx | awk -vORS="" -vOFS="" \
-	-v active_workspace="$active_workspace" \
-	-v active_window="$active_window" \
-	-v active_left="$actv_win_left" \
-	-v active_right="$actv_win_right" \
-	-v inactive_left="$inactv_win_left" \
-	-v inactive_right="$inactv_win_right" \
-	-v separator="$separator" \
-	-v show="$show" \
-	-v c_case="$char_case" \
-	-v char_limit="$char_limit" \
-	-v add_spaces="$add_spaces" \
-	-v on_click="$0" \
-	-v max_windows="$max_windows" \
-	'{
-	if ($2 != active_workspace && $2 != "-1") { next }
-	if ($3 ~ "polybar" || $3 ~ "yad") { next }
-	if (window_count >= max_windows) {
-		hidden_windows++
-		next
-	}
+get_active_workspace() {
+	wmctrl -d |
+		while IFS="[ .]" read -r number active_status _; do
+			test "$active_status" = "*" && echo "$number" && break
+		done
+}
 
-	if (show == "window_class") {
-		lastitem=split($3,classname_and_class,".")
-		displayed_name = classname_and_class[lastitem]
-	}
-	else if (show == "window_classname") {
-		split($3,classname_and_class,".")
-		displayed_name = classname_and_class[1]
-	}
-	else if (show == "window_title") {
-		# format window title from wmctrl output
-		title = ""
-		for (i = 5; i <= NF; i++) {
-			title = title $i
-			if (i != NF) { title = title " "}
-		}
-		displayed_name = title
-	}
+get_active_wid() {
+	active_wid=$(xprop -root _NET_ACTIVE_WINDOW)
+	echo 0x0${active_wid#*x}
+}
 
-	if      (c_case == "lower") { displayed_name = tolower(displayed_name) }
-	else if (c_case == "upper") { displayed_name = toupper(displayed_name) }
+generate_window_list() {
+	active_workspace=$(get_active_workspace)
+	active_wid=$(get_active_wid)
+	window_count=0
+	on_click="$0"
+	echo ""
+	while IFS="[ .\.]" read -r wid ws cname cls host title; do
+		# Don't show windows on another workspace (-1 = sticky)
+		if [ "$ws" != "$active_workspace" ] && [ "$ws" != "-1" ]; then
+			continue
+		fi
 
-	if (length(displayed_name) > char_limit) {
-		displayed_name = substr(displayed_name,1,char_limit)"…"
-	}
+		# Don't show forbidden classes
+		case "$forbidden_classes" in
+			*$cls*) continue ;;
+		esac
 
-	if (add_spaces == "true") { displayed_name = " "displayed_name" " }
+		# If max number of windows reach, just increment
+		# the windows counter
+		if [ "$window_count" -ge "$max_windows" ]; then
+			window_count=$(( window_count + 1 ))
+			continue
+		fi
+		
+		# Show the user-selected window property
+		case "$show" in
+			"window_class") w_name="$cls" ;;
+			"window_classname") w_name="$cname" ;;
+			"window_title") w_name="$title" ;;
+		esac
+		
+		# Use user-selected character case
+		case "$char_case" in
+			"lower") w_name=$(
+				echo "$w_name" | tr '[:upper:]' '[:lower:]'
+				) ;;
+			"upper") w_name=$(
+				echo "$w_name" | tr '[:lower:]' '[:upper:]'
+				) ;;
+		esac
 
-	if ($1 == active_window) {
-		displayed_name=active_left displayed_name active_right
-	}
-	else {
-		displayed_name=inactive_left displayed_name inactive_right
-	}
+		# Truncate displayed name to user-selected limit
+		if [ "${#w_name}" -gt "$char_limit" ]; then
+			w_name="$(echo "$w_name" | cut -c1-$((char_limit-1)))…"
+		fi
 
-	if (non_first == "") { non_first = "true" } else {
-		print separator # only on non-first items
-	}
+		# Apply add-spaces setting
+		if [ "$add_spaces" = "true" ]; then
+			w_name=" $w_name "
+		fi
 
-	print "%{A1:"on_click" raise_or_minimize "$1":}"
-	print "%{A2:"on_click" close "$1":}"
-	print "%{A3:"on_click" slop_resize "$1":}"
-	print "%{A4:"on_click" increment_size "$1":}"
-	print "%{A5:"on_click" decrement_size "$1":}"
-	print displayed_name
-	print "%{A}%{A}%{A}%{A}%{A}"
-	window_count++
-	}
+		# Add left and right formatting to displayed name
+		if [ "$wid" = "$active_wid" ]; then
+			w_name="${active_left}${w_name}${active_right}"
+		else
+			w_name="${inactive_left}${w_name}${inactive_right}"
+		fi
 
-	END{ if (hidden_windows>0) { print separator" +"hidden_windows }}')
+		# Add separator before all items except for the first
+		if [ "$window_count" != 0 ]; then
+			printf "%s" "$separator"
+		fi
 
-echo "$window_list"
+		# Add on-click action Polybar formatting
+		printf "%s" "%{A1:$on_click raise_or_minimize $wid:}"
+		printf "%s" "%{A2:$on_click close $wid:}"
+		printf "%s" "%{A3:$on_click slop_resize $wid:}"
+		printf "%s" "%{A4:$on_click increment_size $wid:}"
+		printf "%s" "%{A5:$on_click decrement_size $wid:}"
+		printf "%s" "$w_name"
+		printf "%s" "%{A}%{A}%{A}%{A}%{A}"
+
+		window_count=$(( window_count + 1 ))
+	done <<-EOF
+	$(wmctrl -lx)
+	EOF
+
+	# Print number of hidden windows
+	if [ "$window_count" -gt "$max_windows" ]; then
+		printf "%s" "+$(( window_count - max_windows ))"
+	fi
+}
+
+# Print a new window list every time the active window changes or a window
+# is opened or closed
+xprop -root -spy _NET_CLIENT_LIST _NET_ACTIVE_WINDOW | while IFS= read _; do
+	generate_window_list
+done
+
+
+
